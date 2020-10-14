9da3b3261c90a3e26113ebf5a05eddd1d93778ba
S. Aziz <azizecomm@gmail.com>
2020-10-14 15:20:49 +0800

    add comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,7 @@
+/**
+ * App.tsx
+ * - TypeScript XML file
+ * - Specifies the App component
+ */
+
 const App = () => <div>Hello React App from scratch</div>;
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,15 @@
+/**
+ * index.tsx
+ * - a TypeScript XML file
+ * - sets up rendering of our React app using `<App />`
+ */
+
+// The important thing to call out here is that this does not use a module system or module loader
+// This concatenates the output of the files into one, so there is no support for import or export syntax
+// Instead, you must use triple-slash directives to reference other TypeScript files
+// ... and global dependencies must be included in the HTML page.
+//
+// Please have a look at `"compilerOptions": { "module": "None",` in tsconfig.json for settings
 /// <reference path="App.tsx" />
 
 ReactDOM.render(<App />, document.getElementById("root"));
